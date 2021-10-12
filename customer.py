7a6576aada9159bec9ae6ffe09e11d7eda21d873
Your Name <charles.luas.young@gmail.com>
2021-10-12 15:49:33 -0600

    Finished classes

--- customer.py
+++ customer.py
@@ -8,3 +8,26 @@
     c.  As a developer, I want the Customer class to have a method to view all products in the customer’s shopping cart. (Loop over the shopping cart’s 
         products list and print each product to the terminal)
 '''
+
+from shopping_cart import ShoppingCart
+
+class Customer:
+    def __init__(self):
+        self.customer_name = ""
+        self.customer_cart = ""
+
+    def customers_name (self):
+        self.customer_name = input("Enter your first and last name: ")
+        return self.customer_name
+
+    def customer_add_cart (self, product):
+        add_product = ShoppingCart()
+        return add_product.add_to_cart(product)
+        
+    def customer_view_cart(self):
+        customer_cart = ShoppingCart()
+        print_customer_cart = customer_cart.product_list
+        for item in print_customer_cart:
+            print(item)
+
+
